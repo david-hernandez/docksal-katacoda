3ec56141b25e0d9b9468e13b41c7c0a19809ab11
davidhernandez <david@narrabilis.com>
2018-06-11 14:02:11 -0400

    Updated

--- composer/env-init.sh
+++ composer/env-init.sh
@@ -1,3 +1,3 @@
-apt-get install php7.0 php7.0-mbstring php7.0-xml php7.0-curl -y &
-curl -sS https://getcomposer.org/installer | php &
+apt-get install php7.0 php7.0-mbstring php7.0-xml php7.0-curl -y &&
+curl -sS https://getcomposer.org/installer | php &&
 mv composer.phar /usr/local/bin/composer
